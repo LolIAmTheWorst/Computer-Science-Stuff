c22415a992dc43098b44f8c1744f109bd2e963e1
LolIAmTheWorst <36773008+LolIAmTheWorst@users.noreply.github.com>
2021-02-24 09:23:45 -0500

    Create HelloWorld.java

---
+++ Basics/HelloWorld.java
@@ -0,0 +1,6 @@
+class Main {
+  public static void main(Strings[] args) {
+    System.out.println("Hello world");
+    //System.out.print("Hello world"); makes no new line after writing hello world
+  }
+}
