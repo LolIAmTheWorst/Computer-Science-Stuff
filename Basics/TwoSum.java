f05465dfbb9d301966c3517d1edc16125bb03060
LolIAmTheWorst <36773008+LolIAmTheWorst@users.noreply.github.com>
2021-02-24 09:26:36 -0500

    Update TwoSum.java
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.

--- Basics/TwoSum.java
+++ Basics/TwoSum.java
@@ -1,3 +1,16 @@
+
+/*
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. 
+
+*/
+
+// The method I decided to use to solve this problem was a bruteforce method.
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
        for (int i = 0; i < nums.length; i++) {
